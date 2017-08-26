5af825f04789bf850ff1c8f43b5eaed16661f40b
paulmillen <pwmillen@gmail.com>
2017-08-26 20:22:03 +0100

    added authenticated user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :albums
+  resources :profiles
 
 
   get 'home/index'
+
+  authenticated :user do
+    root to: 'profiles#show', as: :authenticated_root
+  end
+  
   root 'home#index'
+
+
 
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -5,6 +5,19 @@ RSpec.feature 'User' do
     expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
+  it 'redirects to a users profile' do
+    sign_up
+    expect(current_path).to eq('/')
+    expect(page).to have_content "Alice's Profile"
+  end
+
+  it 'displays another homepage for unauthenticated visitors' do
+    sign_up
+    click_button 'Logout'
+    expect(page).not_to have_content "Alice's profile"
+    expect(page).to have_content "hello world"
+  end
+
   describe 'log out button' do
     it 'allows signed-in user to sign out' do
       sign_up
