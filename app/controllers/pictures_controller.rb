7c57bf872f5d9699aeb2a52b3330347be4b98462
paulmillen <pwmillen@gmail.com>
2017-08-27 14:31:29 +0100

    user's pictures display on profile page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -23,7 +23,7 @@ class PicturesController < ApplicationController
   private
 
   def picture_params
-    params.require(:picture).permit(:title, :description, :image)
+    params.require(:picture).permit(:title, :description, :image).merge(user_id: current_user.id)
   end
 
 end
