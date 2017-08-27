7c62bba2de26da0ccd5d2a060ae93e7062391763
paulmillen <pwmillen@gmail.com>
2017-08-27 12:29:17 +0100

    user can add pictures

--- spec/features/picture_spec.rb
+++ spec/features/picture_spec.rb
@@ -2,9 +2,9 @@ RSpec.feature 'Picture' do
 
   it 'allows user to upload a picture' do
     visit 'pictures/new'
-    attach_file('photo_image', Rails.root + 'spec/files/test.png', visible: false)
+    attach_file('picture_image', Rails.root + 'spec/files/test.png', visible: false)
     click_button 'Upload'
-    expect(Pictures.count).to eq 1
+    expect(Picture.count).to eq 1
   end
 
 end
